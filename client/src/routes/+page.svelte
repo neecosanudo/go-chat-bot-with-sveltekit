130604b7b27dda34701bb7710820f3936cc46627
Nicolás Sañudo <nicolas.sanudo2@gmail.com>
2022-09-09 20:47:32 -0300

    ✨ Create Client with SvelteKit

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  const URL = 'http://127.0.0.1:5050/';
+
+  let home = fetch(URL).then(
+    res => res.json()
+  )
+</script>
+
+<svelte:head>
+  <title>Bot</title>
+</svelte:head>
+
+{#await home}
+  <p>Loading home</p>
+{:then data} 
+  <h1>{data.message}</h1>
+{/await}
